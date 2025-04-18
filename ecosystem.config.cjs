cd589cd6738891d1f5c86084cbbba2b2a7c9ebdc
Luis Pfuño Canales <luispfcanales@gmail.com>
2025-04-18 14:35:03 -0500

    port all

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,8 @@ module.exports = {
       PM2_SERVE_PATH: "./dist",
       PM2_SERVE_PORT: 9001,
       PM2_SERVE_SPA: "true",
-      PM2_SERVE_HOMEPAGE: "/index.html"
+      PM2_SERVE_HOMEPAGE: "/index.html",
+      PM2_SERVE_HOST: "0.0.0.0"
     }
   }]
 }
