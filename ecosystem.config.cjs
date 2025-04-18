2c8e57c2f74025fa6b56a76a13ee9b48f02ad894
Luis Pfuño Canales <luispfcanales@gmail.com>
2025-04-18 17:55:38 -0500

    change port

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
     script: "serve",
     env: {
       PM2_SERVE_PATH: "./dist",
-      PM2_SERVE_PORT: 9001,
+      PM2_SERVE_PORT: 8095,
       PM2_SERVE_SPA: "true",
       PM2_SERVE_HOMEPAGE: "/index.html",
       PM2_SERVE_HOST: "0.0.0.0"
